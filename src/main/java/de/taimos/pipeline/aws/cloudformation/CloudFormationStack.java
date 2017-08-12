2bd55d342cfe6cb3e3a57272d44ef14d28295a69
Elliot Murphy <statik@users.noreply.github.com>
2017-08-12 08:26:45 -0400

    Simplify from code review.
    Signed-off-by: Elliot Murphy <elliot@elliotmurphy.com>

--- src/main/java/de/taimos/pipeline/aws/cloudformation/CloudFormationStack.java
+++ src/main/java/de/taimos/pipeline/aws/cloudformation/CloudFormationStack.java
@@ -58,7 +58,7 @@ public class CloudFormationStack {
 			DescribeStacksResult result = this.client.describeStacks(new DescribeStacksRequest().withStackName(this.stack));
 			return !result.getStacks().isEmpty();
 		} catch (AmazonCloudFormationException e) {
-			if ("AccessDenied".compareTo(e.getErrorCode()) == 0) {
+			if ("AccessDenied".equals(e.getErrorCode())) {
 				this.listener.getLogger().format("Got error from describeStacks: %s\n", e.getErrorMessage());
 				throw e;
 			}
