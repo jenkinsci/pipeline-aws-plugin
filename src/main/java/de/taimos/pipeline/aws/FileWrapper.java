b5cefc008521e5f9f4248c58c2e3f433ab61d606
Thorsten Hoeger <thorsten.hoeger@taimos.de>
2017-10-10 20:46:33 +0200

    code cleanup

--- src/main/java/de/taimos/pipeline/aws/FileWrapper.java
+++ src/main/java/de/taimos/pipeline/aws/FileWrapper.java
@@ -29,12 +29,34 @@
 
 package de.taimos.pipeline.aws;
 
-import hudson.FilePath;
-import org.jenkinsci.plugins.scriptsecurity.sandbox.whitelists.Whitelisted;
+/*-
+ * #%L
+ * Pipeline: AWS Steps
+ * %%
+ * Copyright (C) 2016 - 2017 Taimos GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 
-import javax.annotation.Nonnull;
 import java.io.IOException;
 import java.io.Serializable;
+
+import javax.annotation.Nonnull;
+
+import org.jenkinsci.plugins.scriptsecurity.sandbox.whitelists.Whitelisted;
+
+import hudson.FilePath;
 
 /**
  * Contains serializable information about a file name.
--- src/test/java/de/taimos/pipeline/aws/WithAWSStepTest.java
+++ src/test/java/de/taimos/pipeline/aws/WithAWSStepTest.java
@@ -1,5 +1,35 @@
 package de.taimos.pipeline.aws;
 
+/*-
+ * #%L
+ * Pipeline: AWS Steps
+ * %%
+ * Copyright (C) 2016 - 2017 Taimos GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
 import com.cloudbees.hudson.plugins.folder.AbstractFolder;
 import com.cloudbees.hudson.plugins.folder.Folder;
 import com.cloudbees.hudson.plugins.folder.properties.FolderCredentialsProvider;
@@ -10,16 +40,8 @@ import com.cloudbees.plugins.credentials.SystemCredentialsProvider;
 import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials;
 import com.cloudbees.plugins.credentials.domains.Domain;
 import com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl;
-import hudson.util.ListBoxModel;
-import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
-import org.jenkinsci.plugins.workflow.job.WorkflowJob;
-import org.junit.Assert;
-import org.junit.ClassRule;
-import org.junit.Test;
-import org.jvnet.hudson.test.JenkinsRule;
 
-import java.util.ArrayList;
-import java.util.List;
+import hudson.util.ListBoxModel;
 
 /**
  * Test the behavior of the {@link WithAWSStep}
